01e2917e0526d4a63534aaa8a8ec60ff15b3c04b
Elizabeth <kn-elizaweta@yandex.ru>
2023-09-17 12:17:00 +0300

    Добавлено создание пользователя

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+  def create
+    user = User.create!(user_params)
+    render json: user
+  end
+
   def cinemas_name
     cinemas = Cinema.joins(movies: [sessions: [viewers: :user]]).
                       where(users: {id: params[:id]}).map{ |cinema| cinema.name }
     render json: cinemas
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :email)
   end
 end
